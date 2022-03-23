6fd19f52df9833819fbdb10262801c99c5b6758f
William Mueller <william.mueller@bishopireton.org>
2022-03-23 14:26:18 -0400

    added models to admin page

--- study/admin.py
+++ study/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Student, Study, Section, Course
+
+admin.site.register(Student)
+admin.site.register(Study)
+admin.site.register(Section)
+admin.site.register(Course)
