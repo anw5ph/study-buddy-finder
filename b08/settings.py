aefd0fe64c47383d9316d12e1dc65c530722c784
Nicholas Talton <91391680+Nick-Talton@users.noreply.github.com>
2022-03-23 11:59:39 -0400

    initial google calendar commit

--- b08/settings.py
+++ b08/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -159,3 +160,11 @@ SITE_ID = 2
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+#calendar key
+GOOGLE_CALENDAR_API_KEY = ""
+
+#calendar names you want to import
+CALENDAR_NAMES = [
+    'Im not sure what to put',
+]
