0513410ba27849a3ab331307c6931e464718ea6b
anw5ph <anw5ph@virginia.edu>
2022-03-25 11:13:02 -0400

    Completed form and page for courses

--- study/urls.py
+++ study/urls.py
@@ -1,14 +1,21 @@
-# from django.urls import path
-# from . import views
+from django.urls import path
+from . import views
 
-# app_name = 'study'
-# urlpatterns = [
-#     # Study sessions for a course
-#     path('<int:course-number>-<int:course-section>/', views.AllSessionView.as_view(), name='all-sessions'),
-#     # Individual study session
-#     path('<int:course-number>-<int:course-section>/session/<int:pk>/', views.SessionView.as_view(), name='session'),
-#     # Add study session
-#     path('<int:course-number>-<int:course-section>/session-add/', views.AddSession.as_view(), name='add-session'),
-#     # Add course
-#     path('course-add/', views.AddCourse.as_view(), name='add-course'),
-# ]
+app_name = 'study'
+urlpatterns = [
+    # Study sessions for a course
+    #path('<int:course-number>-<int:course-section>/', views.AllSessionView.as_view(), name='all-sessions'),
+    # Individual study session
+    #path('<int:course-number>-<int:course-section>/session/<int:pk>/', views.SessionView.as_view(), name='session'),
+    # Add study session
+    #path('<int:course-number>-<int:course-section>/session-add/', views.AddSession.as_view(), name='add-session'),
+    
+    # View courses
+    path('courses/', views.CourseView.as_view(), name='courses'),
+    # Add course
+    path('course-add/', views.CourseAddView.as_view(), name='course-add'),
+
+    # Upload course
+    path('course-add/upload', views.uploadCourse, name='upload'),
+    # path('my-account', views.MyAccountView.as_view(), name='my-account'),
+]
