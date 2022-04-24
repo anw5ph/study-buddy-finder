f6aee3422ac651c198abc4fd3079aaa85cea285c
William Mueller <william.mueller@bishopireton.org>
2022-04-24 16:11:55 -0400

    trying to fix tests

--- b08/settings.py
+++ b08/settings.py
@@ -33,7 +33,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'b08project.herokuapp.com']
 
-SECURE_SSL_REDIRECT = True
+if 'test' not in sys.argv:
+    SECURE_SSL_REDIRECT = True
 
 # Application definition
 
